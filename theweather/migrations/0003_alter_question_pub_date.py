156759cb0a0164d72fb05813a3cd36066d7dd0c1
Hiury <101677023+HiuryPro@users.noreply.github.com>
2023-08-27 13:15:01 -0300

    Correções de Nome do projeto

--- theweather/migrations/0003_alter_question_pub_date.py
+++ theweather/migrations/0003_alter_question_pub_date.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polls', '0002_question_teste'),
+        ('theweather', '0002_question_teste'),
     ]
 
     operations = [
