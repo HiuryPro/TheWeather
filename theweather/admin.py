f9762f8532acb93d7aae2a502ea6f25720dcba0a
joo <jasoaresusa@gmail.com>
2023-09-30 19:58:41 -0300

    teste

--- theweather/admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< Updated upstream
-from django.contrib import admin
-
-from .models import Question
-
-admin.site.register(Question)
-=======
-from django.contrib import admin
->>>>>>> Stashed changes
--- theweather/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TheWeatherConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'theweather'
