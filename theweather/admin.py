4b4ea81c7ebd7fd4a806f8e384e95e72f18ba1be
joo <jasoaresusa@gmail.com>
2023-09-30 19:48:17 -0300

    MUDANDO PARA A MINHA BRANCH

--- theweather/admin.py
+++ theweather/admin.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 from django.contrib import admin
 
 from .models import Question
 
 admin.site.register(Question)
+=======
+from django.contrib import admin
+>>>>>>> Stashed changes
--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("theweather/", include("theweather.urls")),
+    path("", include("theweather.urls")),
     path('admin/', admin.site.urls),
 ]
