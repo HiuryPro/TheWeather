ed7e939886f71cad42cad7ca3ff504d06f87c743
Ricardo Balbino Vaz Filho <ricardobalbinovazfilho@hotmail.com>
2023-10-18 20:37:54 -0300

    alterações graficos

--- theweather/views.py
+++ theweather/views.py
@@ -19,24 +19,53 @@ client = MongoClient(
 db = client.WheatherDB
 regiao = db.regiao
 dados_metereologicos = db.dados_metereologicos
+#  {
+#           label: "My First Dataset",
+#           data: teste,
+#           parsing: {
+#             xAxisKey: 'dt_criacao',
+#             yAxisKey: 'umidade'
+#           }
+#  }
 
 
-class IndexView(generic.ListView):
+def IndexView(request):
     template_name = "theweather/index.html"
     context_object_name = "latest_regiao_list"
 
-    def get_queryset(self):
-        lookup = {"$lookup": {"from": "dados_metereologicos",
-                              "localField": "_id",
-                              "foreignField": "id_regiao",
-                              "as": "dadosM"}}
+    lookup = {"$lookup": {"from": "dados_metereologicos",
+                            "localField": "_id",
+                            "foreignField": "id_regiao",
+                            "as": "dadosM"}}
 
-        pipeline = [lookup]
+    unwind = {"$unwind": "$dadosM"}
 
-        results = list(regiao.aggregate(pipeline))
+    sort = {"$sort": {"dadosM.dt_criacao": 1}}
 
-        """Return the last five published questions."""
-        return results
+    group = {"$group": {"_id": "$_id", 
+                        "regiao": {"$first": "$regiao"}, 
+                        "dadosM": {"$push": "$dadosM"}}}
+
+    project = {"$project": {'_id':0, 'dadosM._id': 0, 'dadosM.id_regiao':0, 'dadosM.regiao':0}}
+
+    pipeline = [lookup, unwind, sort, group, project]
+
+    results = list(regiao.aggregate(pipeline))
+    print(results)
+
+    resultsFilter = []
+
+    for dados in results:
+        for maps in dados['dadosM']:
+            for key, value in maps.items():
+                if (type(value) == Decimal128):
+                    maps[key] = float(str(maps[key]))
+                elif (type(value) == datetime.datetime):
+                    maps[key] = maps[key].strftime('%d/%m/%y %H:%M:%S')
+        resultsFilter.append(dados)
+
+    """Return the last five published questions."""
+    return render(request, template_name, {context_object_name: resultsFilter})
 
 
 def cadastrar_regiao(request):
