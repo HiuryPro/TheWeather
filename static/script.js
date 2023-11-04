9485459a30c29e00231c9eb7dc5116162fa4f507
Hiury <101677023+HiuryPro@users.noreply.github.com>
2023-11-03 22:09:38 -0300

    Gráficos com dados do esp32

--- static/script.js
+++ static/script.js
@@ -15,6 +15,7 @@ function graficoLine(ctx, dataset, title, font){
           }
       },
         scales: {
+          
             x: {
                 type: 'time',
                 time: {
@@ -36,33 +37,69 @@ function criaGraficosUmTemp(dict){
     console.log(regioes[i])
   }
 
+  console.log(regioes.length)
+  console.log()
+
+  const rowNumbers = (regioes.length / 2) + (regioes.length % 2)
+  var count = 1
 
   const section = document.getElementById("section")
 
-  const row = document.createElement("div");
-  row.classList = ["row"]
-
-
-
-  const col = document.createElement("div")
-  col.classList = ["col-md-6"]
-
-
-
-  const divC =  document.createElement("div")
-  divC.classList = ["chart-container"]
-  divC.style = "aspect-ratio: 2"
+  for(var i = 0; i < rowNumbers; i++){
+    const row = document.createElement("div");
+    row.classList = ["row"]
   
-  const canvas = document.createElement("canvas")
-  canvas.id = "myChart3"
+    console.log("Funciona");
+  
+    const col1 = document.createElement("div")
+    col1.classList = ["col-md-6"]
+
+    const col2 = document.createElement("div")
+    col2.classList = ["col-md-6"]
+  
+  
+  
+    const divC1 =  document.createElement("div")
+    divC1.classList = ["chart-container"]
+    divC1.style = "aspect-ratio: 2"
+
+    const divC2 =  document.createElement("div")
+    divC2.classList = ["chart-container"]
+    divC2.style = "aspect-ratio: 2"
+    
+    const canvas1 = document.createElement("canvas")
+    canvas1.id = "UT" + count
+
+    count++
+    const canvas2 = document.createElement("canvas")
+    canvas2.id = "UT" + count
+    count++
+  
+    divC1.appendChild(canvas1)
+    col1.appendChild(divC1)
+
+    divC2.appendChild(canvas2)
+    col2.appendChild(divC2)
+
+    row.appendChild(col1)
+    row.appendChild(col2)
+
+    section.appendChild(row)
+  }
+
+  var count = 1
+
+  for (var i in regioes){
+    console.log("UT" + count)
+    const ctx = document.getElementById("UT" + count);
+    console.log(dict[regioes[i]])
+    console.log(regioes[i])
+    graficoLine(ctx, dict[regioes[i]],`Gráfico Temperatura/Umidade ${regioes[i]} (2023)`,20)
+    count++
+
+  }
 
 
-  divC.appendChild(canvas)
-  col.appendChild(divC)
-  row.appendChild(col)
 
-
-  section.appendChild(row)
-
-  console.log("Funciona")
+  console.log("Funciona2")
 }
--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,1 +1,35 @@
-console.log('Funciona')
+function graficoLine(ctx, dataset, title, font){
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        datasets: dataset,
+      },
+      options: {
+        plugins: {
+          title: {
+              display: true,
+              text: title,
+              font: {
+                size: font
+            }
+          }
+      },
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'day',
+                    displayFormats: {
+                        day: 'dd/MM', // Format for date and time
+                    },
+                }
+            }
+           
+        }
+    }
+    });
+}
+
+function criaGraficosUmTemp(dict){
+  console.log(dict)
+}
