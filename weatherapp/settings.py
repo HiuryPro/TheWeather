922a112c502dde191279221f2665d3a169a5a79b
joo <jasoaresusa@gmail.com>
2023-11-14 12:52:40 -0300

    Imagens no gráfico

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -112,6 +112,9 @@ DATE_INPUT_FORMATS = ["%d/%m/%Y"]
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
